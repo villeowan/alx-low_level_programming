fbdfda01942df056700b242ebdfb26c15fd41e33
Nevil Wandera <villeowan@gmail.com>
2022-12-12 12:30:38 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - prints all possible combinations of two two digits
  *
  * Return: Always 0 (Success)
  */
@@ -24,13 +24,13 @@ int main(void)
 
 			if (x == 98 && y == 99)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
 }
-			
