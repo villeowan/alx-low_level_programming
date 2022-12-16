2e6fee050eac63fad32a265cb689686096a4383f
Nevil Wandera <villeowan@gmail.com>
2022-12-16 07:44:04 -0800

    changed main.c to main.h

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,21 @@
-#include "main.c"
+#include "main.h"
 
 /**
- * print_square - prints a square
- * followed by a new line
- * @size: input integer
- * Return: Always 0
+ * print_square - prints a square followed by a new line
+ * @size: size of the square
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_square(int size)
 {
-	int i, j;
+	int k, l;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (k = 0; k < size; k++)
 		{
-			for (j = 0; j < size; j++)
+			for (l = 0; l < size; l++)
 				_putchar('#');
 			_putchar('\n');
 		}
