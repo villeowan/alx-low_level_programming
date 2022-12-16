3203f670d7554f9f1317f0b36010c794e16e5b0a
Nevil Wandera <villeowan@gmail.com>
2022-12-16 07:31:11 -0800

    changed variables (k,l) to (i,j)

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,13 @@
 
 void print_square(int size)
 {
-	int k, l;
+	int i, j;
 
 	if (size > 0)
 	{
-		for (k = 0; k < size; k++)
+		for (i = 0; i < size; i++)
 		{
-			for (l = 0; l < size; l++)
+			for (j = 0; j < size; j++)
 				_putchar('#');
 			_putchar('\n');
 		}
