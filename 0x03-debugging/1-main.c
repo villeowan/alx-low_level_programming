5bf1fc6e036bc3507a2ab253dd16e46d313d8945
Nevil Wandera <villeowan@gmail.com>
2022-12-16 12:08:44 -0800

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: Always 0 (Success)
+ * Return: 0;
  */
 
 int main(void)
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)*/
-	/* {*/
-	/*	putchar(i);*/
-	/* }*/
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
