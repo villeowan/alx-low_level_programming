426f27ba879184411d5ec21178978dfdb61f7199
Nevil Wandera <villeowan@gmail.com>
2022-12-21 09:48:58 -0800

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,22 +11,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srcl = 0, k = 0;
-	char *t = dest, *s = src;
+	int index = 0, srcl = 0;
 
-	while (*src)
-	{
+	while (src[index++])
 		srcl++;
-		src++;
-	}
-
-	srcl++;
-
-	if (n > srcl)
-		n = srcl;
-	src = s;
-
-	for (; k < n; k++)
-		*dest++ = *src++;
-	return (t);
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = srcl; index < n; index++)
+		dest[index] = src[index];
+	for (index = srcl; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
 }
