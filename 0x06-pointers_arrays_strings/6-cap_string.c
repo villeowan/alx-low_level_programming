0e90a172f28fb65e30ffcb5f2462f9f0982be040
Nevil Wandera <villeowan@gmail.com>
2022-12-22 08:36:04 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,35 +3,32 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * @k: input string
+ * @str: string to capitalize
  * Return: char pointer converted string
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int k = 0;
 
-	while (str[k])
+	while (str[++k])
 	{
 		while (!(str[k] >= 'a' && str[k] <= 'z'))
 			k++;
-		if (str[k - 1] == ' ' || 
-		    str[k - 1] == '\t' || 
-		    str[k - 1] == '\n' || 
-		    str[k - 1] == ',' || 
-		    str[k - 1] == ';' || 
-		    str[k - 1] == '.' || 
-		    str[k - 1] == '!' || 
-		    str[k - 1] == '?' || 
-		    str[k - 1] == '"' || 
-		    str[k - 1] == '(' || 
-		    str[k - 1] == ')' || 
-		    str[k - 1] == '{' || 
-		    str[k - 1] == '}' || 
-		    k == 0)
+		if (str[k - 1] == ' ' ||
+				str[k - 1] == '\t' ||
+				str[k - 1] == '\n' ||
+				str[k - 1] == ',' ||
+				str[k - 1] == ';' ||
+				str[k - 1] == '.' ||
+				str[k - 1] == '!' ||
+				str[k - 1] == '?' ||
+				str[k - 1] == '"' ||
+				str[k - 1] == '(' ||
+				str[k - 1] == ')' ||
+				str[k - 1] == '{' ||
+				str[k - 1] == '}')
 			str[k] -= 32;
-		k++;
 	}
-
 	return (str);
 }
